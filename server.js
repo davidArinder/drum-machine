7a4209f1612bfc873848a2b75cbd866f507504ea
David Arinder <david.arinder@gmail.com>
2019-03-08 20:01:42 -0800

    servers

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+const server = express();
+const PORT = 8080;
+
+server.listen(PORT);
+console.log(`listening on port: ${PORT}`);
+
+server.use(express.static(__dirname));
+server.get('/drum-machine', (req, res) => {
+  return res.sendFile(__dirname + '/fcc-drum-machine-css/index.html');
+});
