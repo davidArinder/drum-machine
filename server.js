0b2c9cf4be8a581840baa99336b4bfc2063a1374
David Arinder <34454124+davidArinder@users.noreply.github.com>
2019-04-11 12:48:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const server = express();
 
 server.listen(process.env.PORT || 8080);
-server.use(express.static(__dirname));
+server.use(express.static(__dirname)); // needed for Heroku deployment
 server.get('/drum-machine', (req, res) => {
   return res.sendFile(__dirname + '/public/index.html');
 });
